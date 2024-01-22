ddf5c51faa03385d275bb34ce53d6e15d959fc3c
Sanni-Damilola <sannifortune11@gmail.com>
2024-01-22 17:46:31 +0100

    calculator

--- calculator.py
+++ calculator.py
@@ -2,8 +2,8 @@ num1 = input("Enter a Number: ")
 num2 = input("Enter a Number: ")
 
 # Convert input values to numbers
-num1 = int(num1)
-num2 = int(num2)
+num1 = float(num1)
+num2 = float(num2)
 
 result = num1 + num2
 print(result)
