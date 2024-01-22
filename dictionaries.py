48cf53bf6a407362e026c6f4c3564b2f3ec179ce
Sanni-Damilola <sannifortune11@gmail.com>
2024-01-22 20:46:15 +0100

    dictionaries

---
+++ dictionaries.py
@@ -0,0 +1,16 @@
+monthConversions = {
+    "Jan": "January",
+    "Feb": "february",
+    "Mar": "March",
+    "Apr": "April",
+    "May": "May",
+    "Jun": "June",
+    "Jul": "July",
+    "Aug": "August",
+    "Sep": "September",
+    "Oct": "October",
+    "Nov": "November",
+    "Dec": "December",
+}
+print((monthConversions))
+
