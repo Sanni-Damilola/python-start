db12c987e2bd06651f901a33b731eb8aaec0ec72
Sanni-Damilola <sannifortune11@gmail.com>
2024-01-22 17:45:55 +0100

    calculator

---
+++ calculator.py
@@ -0,0 +1,9 @@
+num1 = input("Enter a Number: ")
+num2 = input("Enter a Number: ")
+
+# Convert input values to numbers
+num1 = int(num1)
+num2 = int(num2)
+
+result = num1 + num2
+print(result)
--- get_input_from_users.py
+++ get_input_from_users.py
@@ -1,3 +1,4 @@
 store_name = input("Enter Your Name: ")
 age = input("Enter Your Age: ")
-print("Hello " + store_name + "! You are " +  age )
+print("Hello " + store_name + "! You are " + age )
+
