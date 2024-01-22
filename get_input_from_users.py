f76a63cc0b611863e96930e1e0c0e504600e54bb
Sanni-Damilola <sannifortune11@gmail.com>
2024-01-22 17:38:48 +0100

    numbers

---
+++ get_input_from_users.py
@@ -0,0 +1,2 @@
+store_name = input("Enter Your Name: ")
+print("Hello " + store_name + "!" )
--- numbers.py
+++ numbers.py
@@ -1,3 +1,10 @@
+from  math import *
+
 my_num = -5
 print(abs(my_num))
 print(pow(4, 2))
+print(max(100, 6))
+print(min(100, 6))
+print(round(3.5))
+print(round(3.4))
+
