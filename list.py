fd9275fdde6bda7917d00a0e69d9fa53fd1ce4f8
Sanni-Damilola <sannifortune11@gmail.com>
2024-01-22 18:11:20 +0100

    list

--- list.py
+++ list.py
@@ -4,12 +4,20 @@
 # print(family[1:3])
 # print(family)
 #
-luccky_numbers = [4,2,12,56,89,43]
+luccky_numbers = [4,2,12,56,89,43, 4]
 family = ["Sanni", "Ajose", "Idowu", "Babafemi", "Adele"]
-family.extend(luccky_numbers) # join 2 list
-family.append("Obadina") # add an item
-family.insert(1,"test") # add element in a specific index
-family.remove("test") # remove element
-# family.clear() # empty the array
-
-print(family)
+# family.extend(luccky_numbers) # join 2 list
+# family.append("Obadina") # add an item
+# family.insert(1,"test") # add element in a specific index
+# family.remove("test") # remove element
+# # family.clear() # empty the array
+# family.pop() # remove the last element
+# print(family.index("Sanni"))
+# print(family.count(4))
+family.sort()
+luccky_numbers.reverse()
+luccky_numbers.sort()
+family2 = family.copy()
+# print(family)
+print(luccky_numbers)
+print(family2)
