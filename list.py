772abb2d58b453b6fe41dbf03f90f50ffc22d040
Sanni-Damilola <sannifortune11@gmail.com>
2024-01-22 18:00:47 +0100

    list

--- list.py
+++ list.py
@@ -1,2 +1,10 @@
-family = ["Sanni", "Ajose", "Idowu"]
-print(family)
+# family = ["Sanni", "Ajose", "Idowu", "Babafemi", "Adele"]
+# family[0] = "Latteef"
+#
+# print(family[1:3])
+# print(family)
+#
+luccky_numbers = [4,2,12,56,89,43]
+family = ["Sanni", "Ajose", "Idowu", "Babafemi", "Adele"]
+
+print(luccky_numbers)
