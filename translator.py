607bd650a132e1cc77740a8ff5c52b5510ad0766
Sanni-Damilola <sannifortune11@gmail.com>
2024-01-23 13:07:10 +0100

    translate

--- translator.py
+++ translator.py
@@ -2,10 +2,9 @@ def translate(phrase):
     translation = ""
     for value in phrase:
         if value in "AEIOUaeiou":
-        translation = translation + "g"
-    else:
-        translation = translation + value
-        return  translation
+            translation = translation + "g"
+        else:
+            translation = translation + value
+    return translation
 
-
-print(translate())
+print(translate(input("Enter A Word ")))
