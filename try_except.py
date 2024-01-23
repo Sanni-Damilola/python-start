16523eaa570b229c3e9598e398abd9aeaeb002c0
Sanni-Damilola <sannifortune11@gmail.com>
2024-01-23 13:19:04 +0100

    try and except

--- try_except.py
+++ try_except.py
@@ -1,7 +1,9 @@
 
 try:
+    value = 10/0
     numbers = int(input("Enter a number: "))
     print(numbers)
-except:
+except ZeroDivisionError:
+    print("Divided by Zero")
+except ValueError:
     print("Invalid Input")
-
