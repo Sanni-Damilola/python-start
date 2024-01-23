6a6c74cafb2d15d809589b77a5e2a7e16377c960
Sanni-Damilola <sannifortune11@gmail.com>
2024-01-23 12:44:24 +0100

    for loop

--- for_loop.py
+++ for_loop.py
@@ -1,2 +1,13 @@
-for letter in "Volatic Academy":
-    print(letter)
+family = ["Ope", "Emmanuel", "Joseph"]
+
+for name in family:
+    print(name)
+
+for index in range(len(family)):
+    print(family[index])
+
+for index in range(5):
+    if index == 0:
+        print("First Iteration")
+    else:
+        print("Not Fsrst")
