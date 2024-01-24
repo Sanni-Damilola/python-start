d9e6d2ec38c6bea25261e7e5b997fba8feb638d9
Sanni-Damilola <sannifortune11@gmail.com>
2024-01-24 02:05:26 +0100

    classes

--- app.py
+++ app.py
@@ -1,5 +1,29 @@
 from classes_and_objects import student
-
+from question import Question
 
 student1 = student("Sanni", "Backend Developer", 4.5, False)
 student2 = student("Bola", "Software Developer", 2.3, True)
+question_prompts = [
+    "What Color are Apples?\n(a) Red/Green\n(b) Purple\n(c) Green",
+    "What Color are Bananas?\n(a) Magenta\n(b) Teal\n(c) Yellow",
+    "What Color are strawberries?\n(a) Red\n(b) Yellow\n(c) Blue"
+]
+
+questions = [
+    Question(question_prompts[0], "a"),
+    Question(question_prompts[1], "c"),
+    Question(question_prompts[2], "b")
+]
+
+def run_test(questions):
+    score = 0
+    total_questions = len(questions)
+
+    for question in questions:
+        answer = input(question.prompt + "\nYour answer: ")
+        if answer.lower() == question.answer.lower():
+            score += 1
+
+    print(f"You got {score}/{total_questions} correct.")
+
+run_test(questions)
---
+++ question.py
@@ -0,0 +1,5 @@
+class Question:
+    def __init__(self, prompt, answer):
+        self.prompt = prompt
+        self.answer = answer
+
