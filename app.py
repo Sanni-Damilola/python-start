e69fb2d2d5e9e32b4027acbc5310a4dd66d2f3cc
Sanni-Damilola <sannifortune11@gmail.com>
2024-01-24 01:42:06 +0100

    class

--- app.py
+++
@@ -1,7 +0,0 @@
-name = "Sanni"
-print(name.upper().isupper())
-
-print((len(name)))
-print(name[0])
-print(name.lower().index("nni"))
-print(name.replace("Sanni","Fortune"))
---
+++ classes_and_objects.py
@@ -0,0 +1,6 @@
+class student:
+    def __init__(self, name, major, grade, is_on_probation):
+        self.name = name
+        self.major = major
+        self.grade = grade
+        self.is_on_probation = is_on_probation
